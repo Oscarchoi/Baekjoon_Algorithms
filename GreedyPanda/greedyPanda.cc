2bf74bc333fe5797719012288f96dd7893813b04
wooyoung_choi <wychoi502@gmail.com>
2019-12-04 00:48:14 +0900

    wgreedyPanda init

---
+++ GreedyPanda/greedyPanda.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+
+
+int main(){
+  
+  int N;
+  cin>>N;
+  
+  int forest[N][N];
+  int life
+  for(int i=0; i<N; ++i) for(int j=0; j<N; ++j) cin>>forest[i][j];
+
+  for(int i=0; i<
+
+  
+
+  return 0;
+}
