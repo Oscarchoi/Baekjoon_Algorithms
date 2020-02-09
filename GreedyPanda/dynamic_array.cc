be90f63134d19e499c603835eb789bd3f43340b4
wooyoung_choi <wychoi502@gmail.com>
2020-02-09 18:13:39 +0900

    wine_testing

---
+++ GreedyPanda/dynamic_array.cc
@@ -0,0 +1,8 @@
+int* data;
+
+int main(){
+  int n = 5;  
+  data = new int[n]{};
+
+  return 0;
+}
--- WineTasting/wine_tasting.cc
+++ WineTasting/wine_tasting.cc
@@ -2,24 +2,20 @@
 using namespace std;
 
 int N;
-int wine[10000];
-int fc[10000], nc[10000];
+int wine[10001], fc[10001], nc[10001];
 
 int main(){
-
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   cin>>N;
-  for(int i=0;i<N;++i) cin>>wine[i];
-  fc[0]=wine[0];
-  nc[1]=fc[0];
-  fc[1]=fc[0]+wine[1];
+  for(int i=1;i<N+1;++i) cin>>wine[i];
+  fc[1]=wine[1];
   
-  for(int i=2; i<N; ++i){
-    nc[i] = max(nc[i-1],fc[i-1]);
+  for(int i=2; i<N+1; ++i){
+    nc[i] = max(max(nc[i-1],fc[i-1]),nc[i-2]+wine[i-1]);
     fc[i] = nc[i-2]+wine[i-1]+wine[i];
-
   }
-
-
-
+  int result = max(max(fc[N],nc[N]),nc[N-1]+wine[N]);
+  cout<<result<<"\n";
   return 0;
 }
